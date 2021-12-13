7338ffb4ad2ae14711d040c54d9d04fa9ea2daf3
Aaron Psujek <ap058838@abcsupply.com>
2021-12-13 12:33:44 -0600

    Move elements up a level

--- micro-fe-element-one/concat.js
+++ micro-fe-element-one/concat.js
@@ -6,7 +6,7 @@ const files = [
 './dist/micro-fe-element-one/polyfills.js',
 './dist/micro-fe-element-one/main.js',
 ]
-await fs.ensureDir('elements')
-await concat(files, 'elements/elOne.js');
-await fs.copyFile('./dist/micro-fe-element-one/styles.css', 'elements/styles.css')
+await fs.ensureDir('../elements')
+await concat(files, '../elements/elOne.js');
+await fs.copyFile('./dist/micro-fe-element-one/styles.css', '../elements/styles.css')
 })()
--- micro-fe-element-two/concat.js
+++ micro-fe-element-two/concat.js
@@ -6,7 +6,7 @@ const files = [
 './dist/micro-fe-element-two/polyfills.js',
 './dist/micro-fe-element-two/main.js',
 ]
-await fs.ensureDir('elements')
-await concat(files, 'elements/elTwo.js');
-await fs.copyFile('./dist/micro-fe-element-two/styles.css', 'elements/styles.css')
+await fs.ensureDir('../elements')
+await concat(files, '../elements/elTwo.js');
+await fs.copyFile('./dist/micro-fe-element-two/styles.css', '../elements/styles.css')
 })()
